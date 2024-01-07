0cf47cd3f0f8f6684c90cdcc10f288cc6381a84a
Agustin Luengo <83189876+agustinluengo@users.noreply.github.com>
2024-01-07 20:05:43 -0300

    Update config.py

--- server/config.py
+++ server/config.py
@@ -15,11 +15,23 @@ def home():
 def reservation():
     return render_template('profile.html')
 
+# Profile Information Page
+@app.route("/profile", methods=['GET', 'POST'])
+def profile():
+    if request.method == 'POST':
+        # Process the form data and update the user profile
+        # You can access form data using request.form['input_name']
+        # Implement logic to update the user's profile information
+        pass
+    return render_template('profile.html')
+
+
 @app.route("/sports")
 def reservation():
     return render_template('sports.html')
 
 
 
-if __name__  == '__main__':
-    app.run(debug=True)
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=8080, debug=True)
+
