1e60c654c62bc51caa074103d11dd35c981d74bb
Agustin Luengo <83189876+agustinluengo@users.noreply.github.com>
2024-01-08 09:31:05 -0300

    Update config.py

--- server/config.py
+++ server/config.py
@@ -1,30 +1,52 @@
 # pip install Flask 
 # pip install flask-wtf
+# pip install flask-sqlalchemy
 
 ### initiating flask ### 
 # export FLASK_APP=config.py (linux/mac)
 # export FLASK_DEBUG=1 (linux/mac)
 # flask run
 
+from flask import Flask, render_template, url_for, flash, redirect
+from flask_sqlalchemy import SQLAlchemy 
+from forms import RegistrationForm, LoginForm
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'gz3nsrkonCAP882Z'
+app.config['SQLAlchemy_DATABASE_URI'] = 'sqlite:///site.db'
+
+db = SQLAlchemy(app)
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(20), unique=True, nullable=False)        
+    username = db.Column(db.String(120), unique=True, nullable=False)
+    image_file = db.Column(db.String(20), unique=True, nullable=False, default='default.jpg')
+    password = db.Column(db.String(20), unique=True, nullable=False)
+    
+
 @app.route("/")
 @app.route("/home")
 def home():
     return render_template('home.html')
 
+# Profile Information Page
+@app.route("/register", methods=['GET', 'POST'])
+def register():
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        flash(f'Account created for {form.username.data}!', 'success')
+        return redirect(url_for('home'))
+    return render_template('profile.html', title='Login', form=form)
+
+@app.route("/login", methods=['GET','POST'])
+def login():
+        form = LoginForm()
+        if form.validate_on_submit():
+        return render_template('login.html', title='Login', form=form)
+
 @app.route("/profile")
 def reservation():
     return render_template('profile.html')
-
-# Profile Information Page
-@app.route("/profile", methods=['GET', 'POST'])
-def profile():
-    if request.method == 'POST':
-        # Process the form data and update the user profile
-        # You can access form data using request.form['input_name']
-        # Implement logic to update the user's profile information
-        pass
-    return render_template('profile.html')
-
 
 @app.route("/sports")
 def reservation():
